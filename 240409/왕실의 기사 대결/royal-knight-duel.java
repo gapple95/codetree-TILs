99f3ac687854e41016955f9dd152f98b3f48aab4
장현준 <50596733+gapple95@users.noreply.github.com>
2024-04-09 11:33:52 +0900

    Title: 왕실의 기사 대결, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240409/왕실의 기사 대결/royal-knight-duel.java
@@ -0,0 +1,379 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static class Knight {
+		int idx, r, c, h, w, k, hp;
+
+		public Knight(int idx, int r, int c, int h, int w, int k) {
+			super();
+			this.idx = idx;
+			this.r = r;
+			this.c = c;
+			this.h = h;
+			this.w = w;
+			this.k = k;
+			this.hp = k;
+
+			for (int i = r; i < r + h; i++) {
+				for (int j = c; j < c + w; j++) {
+					knightMap[i][j] = idx;
+				}
+			}
+		}
+
+		public boolean Right() {
+			int nc = c + w;
+
+			move[idx] = true;
+
+			// 범위 밖
+			if (nc > L)
+				return false;
+
+			boolean flag = true;
+			for (int i = r; i < r + h; i++) {
+				if (map[i][nc] == 2)
+					return false;
+				else if (knightMap[i][nc] != 0)
+					flag = knights[knightMap[i][nc]].Right();
+			}
+
+			return flag;
+		}
+
+		public boolean moveRight() {
+			// 이동 프로세스
+			for (int i = r + h - 1; i >= r; i--) {
+				for (int j = c + w - 1; j >= c; j--) {
+					// 기사 이동
+					// 오른쪽으로 밀기
+					knightTmp[i][j + 1] = knightMap[i][j];
+					if (map[i][j + 1] == 1)
+						k--;
+				}
+			}
+			c = c + 1;
+			return k <= 0 ? true : false;
+		}
+
+		public boolean Left() {
+			int nc = c - 1;
+
+			move[idx] = true;
+
+			// 범위 밖
+			if (nc <= 0)
+				return false;
+
+			boolean flag = true;
+			for (int i = r; i < r + h; i++) {
+				if (map[i][nc] == 2)
+					return false;
+				else if (knightMap[i][nc] != 0)
+					flag = knights[knightMap[i][nc]].Left();
+			}
+
+			return flag;
+		}
+
+		public boolean moveLeft() {
+
+			// 이동 프로세스
+			for (int i = r; i < r + h; i++) {
+				for (int j = c; j < c + w; j++) {
+					// 기사 이동
+					// 왼쪽으로 밀기
+					knightTmp[i][j - 1] = knightMap[i][j];
+					if (map[i][j - 1] == 1)
+						k--;
+				}
+			}
+			c = c - 1;
+			return k <= 0 ? true : false;
+		}
+
+		public boolean Down() {
+			int nr = r + h;
+
+			move[idx] = true;
+
+			// 범위 밖
+			if (nr > L)
+				return false;
+
+			boolean flag = true;
+			for (int i = c; i < c + w; i++) {
+				if (map[nr][i] == 2)
+					return false;
+				else if (knightMap[nr][i] != 0)
+					flag = knights[knightMap[nr][i]].Down();
+			}
+
+			return flag;
+		}
+
+		public boolean moveDown() {
+
+			// 이동 프로세스
+			for (int i = r + h - 1; i >= r; i--) {
+				for (int j = c + w - 1; j >= c; j--) {
+					// 기사 이동
+					// 아래로 밀기
+					knightTmp[i - 1][j] = knightMap[i][j];
+					if (map[i - 1][j] == 1)
+						k--;
+				}
+			}
+			r = r + 1;
+			return k <= 0 ? true : false;
+		}
+
+		public boolean Up() {
+			int nr = r - 1;
+
+			move[idx] = true;
+
+			// 범위 밖
+			if (nr <= 0)
+				return false;
+
+			boolean flag = true;
+			for (int i = c; i < c + w; i++) {
+				if (map[nr][i] == 2)
+					return false;
+				else if (knightMap[nr][i] != 0)
+					flag = knights[knightMap[nr][i]].Up();
+			}
+			r = r - 1;
+			return flag;
+		}
+
+		public boolean moveUp() {
+			// 이동 프로세스
+			for (int i = r; i < r + h; i++) {
+				for (int j = c; j < c + w; j++) {
+					// 기사 이동
+					// 위로 밀기
+					knightTmp[i + 1][j] = knightMap[i][j];
+					if (map[i + 1][j] == 1)
+						k--;
+				}
+			}
+			return k <= 0 ? true : false;
+		}
+
+		public void stay() {
+			for (int i = r; i < r + h; i++) {
+				for (int j = c; j < c + w; j++) {
+					knightTmp[i][j] = idx;
+				}
+			}
+		}
+
+		public void Dead() {
+			for (int i = r; i < r + h; i++) {
+				for (int j = c; j < c + w; j++) {
+					knightTmp[i][j] = 0;
+				}
+			}
+			idx = 0;
+		}
+
+	}
+
+	static int[][] map, knightMap, knightTmp, initMap;
+	static int L, N, Q;
+	static Knight[] knights;
+	static boolean[] move, init;
+	static boolean first;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		StringBuilder sb = new StringBuilder();
+
+		st = new StringTokenizer(br.readLine());
+
+		L = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+		Q = Integer.parseInt(st.nextToken());
+
+		map = new int[L + 2][L + 2];
+		knightMap = new int[L + 2][L + 2];
+		knightTmp = new int[L + 2][L + 2];
+		initMap = new int[L + 2][L + 2];
+
+		for (int i = 0; i < L + 2; i++) {
+			Arrays.fill(map[i], 2);
+		}
+
+		knights = new Knight[N + 1];
+		move = new boolean[N + 1];
+		init = new boolean[N + 1];
+
+		for (int i = 1; i <= L; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 1; j <= L; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+
+		int r, c, h, w, k;
+		for (int i = 1; i <= N; i++) {
+			st = new StringTokenizer(br.readLine());
+			r = Integer.parseInt(st.nextToken());
+			c = Integer.parseInt(st.nextToken());
+			h = Integer.parseInt(st.nextToken());
+			w = Integer.parseInt(st.nextToken());
+			k = Integer.parseInt(st.nextToken());
+			knights[i] = new Knight(i, r, c, h, w, k);
+		}
+
+		int knight;
+		int d;
+		int hp;
+		for (int i = 0; i < Q; i++) {
+			st = new StringTokenizer(br.readLine());
+			knight = Integer.parseInt(st.nextToken());
+			d = Integer.parseInt(st.nextToken());
+
+			switch (d) {
+			case 0: {
+				if (knights[knight].Up()) {
+					hp = knights[knight].k;
+					for (int j = 1; j <= N; j++) {
+						if (!move[j])
+							continue;
+						if (knights[j].moveUp() && j != knight)
+							knights[j].Dead();
+					}
+					knights[knight].k = hp;
+				} else {
+					for (int j = 1; j <= N; j++) {
+						if (move[j])
+							knights[j].stay();
+					}
+				}
+				break;
+			}
+			case 1: {
+				if (knights[knight].Right()) {
+					hp = knights[knight].k;
+					for (int j = 1; j <= N; j++) {
+						if (!move[j])
+							continue;
+						if (knights[j].moveRight() && j != knight)
+							knights[j].Dead();
+					}
+					knights[knight].k = hp;
+
+				} else {
+					for (int j = 1; j <= N; j++) {
+						if (move[j])
+							knights[j].stay();
+					}
+				}
+
+				break;
+			}
+			case 2: {
+				if (knights[knight].Down()) {
+					hp = knights[knight].k;
+					for (int j = 1; j <= N; j++) {
+						if (!move[j])
+							continue;
+						if (knights[j].moveDown() && j != knight)
+							knights[j].Dead();
+					}
+					knights[knight].k = hp;
+
+				} else {
+					for (int j = 1; j <= N; j++) {
+						if (move[j])
+							knights[j].stay();
+					}
+				}
+
+				break;
+			}
+			case 3: {
+				if (knights[knight].Left()) {
+					hp = knights[knight].k;
+					for (int j = 1; j <= N; j++) {
+						if (!move[j])
+							continue;
+						if (knights[j].moveLeft() && j != knight)
+							knights[j].Dead();
+					}
+					knights[knight].k = hp;
+
+				} else {
+					for (int j = 1; j <= N; j++) {
+						if (move[j])
+							knights[j].stay();
+					}
+				}
+				
+				break;
+			}
+			}
+
+			// 가만히 있던 애들도 제자리 지키게 맵 업데이트
+			for (int j = 1; j <= N; j++) {
+				if (!move[j]) {
+					knights[j].stay();
+				}
+			}
+			copyMap();
+			System.arraycopy(init, 0, move, 0, N + 1);
+
+//			System.out.println(knight + ", " + d);
+//			System.out.println("맵");
+//			printMap(map);
+//			System.out.println("기사");
+//			printMap(knightMap);
+			
+			initMap();
+		}
+		
+		int sum = 0;
+		for (int i = 1; i <=N; i++) {
+			if(knights[i].k > 0)
+				sum += knights[i].hp - knights[i].k;
+		}
+		
+		sb.append(sum);
+		System.out.println(sb);
+
+	}
+
+	static void printMap(int map[][]) {
+//		System.out.println();
+		for (int i = 0; i < L + 2; i++) {
+			for (int j = 0; j < L + 2; j++) {
+				System.out.print(map[i][j] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
+
+	static void copyMap() {
+		for (int i = 1; i <= L; i++) {
+			System.arraycopy(knightTmp[i], 0, knightMap[i], 0, L + 2);
+		}
+	}
+
+	static void initMap() {
+		for (int i = 1; i <= L; i++) {
+			System.arraycopy(initMap[i], 0, knightTmp[i], 0, L + 2);
+		}
+	}
+
+}
